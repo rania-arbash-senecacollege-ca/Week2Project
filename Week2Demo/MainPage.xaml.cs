abbf4b87d4b455905f41049f07623789a55d29ac
rania <raniaarbash@hotmail.com>
2021-09-13 15:46:17 -0400

    * .gitignore: Week2 Project-Data Binding
    * Week2Demo.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * Info.plist:
    * AssemblyInfo.cs:
    * Week2Demo.csproj:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * Entitlements.plist:
    * MainActivity.cs:
    * Week2Demo.iOS.csproj:
    * Default.png:
    * Default@2x.png:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * Week2Demo.Android.csproj:
    * Default-568h@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * Default-Portrait@2x.png:
    * LaunchScreen.storyboard:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon29.png:
    * Icon76.png:
    * Icon60.png:
    * Icon58.png:
    * Icon40.png:
    * Icon20.png:
    * Icon80.png:
    * Icon87.png:
    * icon_round.xml:
    * Icon167.png:
    * Icon180.png:
    * Icon152.png:
    * Icon120.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ Week2Demo/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Week2Demo
+{
+    public partial class MainPage : ContentPage
+    {
+        public int counter = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+            //Content = new Label
+            //{
+            //    HorizontalOptions = LayoutOptions.Center,
+            //    VerticalOptions = LayoutOptions.Center,
+            //    Text = "Hello from Code Behind"
+            //};
+        }
+
+        //void btu1_Clicked(System.Object sender, System.EventArgs e)
+        //{
+        //    DisplayAlert("First message", "This is our first Alert", "OK");
+        //}
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            counter++;
+            myLabel.Text = "This Button clicked for " + Convert.ToString(counter);
+        }
+
+        //void Slider_ValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        //{
+        //    myLabel2.Text = "The slider value is " + e.NewValue;
+        //}
+    }
+}
